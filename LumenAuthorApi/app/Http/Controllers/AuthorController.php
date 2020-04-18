12831cfd6265f61d2b26aa1ea02dd7b12edb99cb
Marcos Palacios <m.palacios@geekshubs.com>
2020-04-18 13:01:05 +0200

    Controller method

---
+++ LumenAuthorApi/app/Http/Controllers/AuthorController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class AuthorController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Return autrhos list
+     * @return Response
+     */
+    public function index(){
+
+    }
+
+    /**
+     * Return autrhos list
+     * @return Response
+     */
+    public function store(Request $request){
+
+    }
+
+    /**
+     * Return an author
+     * @return Response
+     */
+    public function show($author){
+
+    }
+
+    /**
+     * Return an author
+     * @return Response
+     */
+    public function update(Request $request,$author){
+
+    }
+
+    /**
+     * Return an author
+     * @return Response
+     */
+    public function desploy($author){
+
+    }
+
+}
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # DATA BASE
 
-## Migrations
+## Creación de datos
+### Migrations
 > database/migrations/2020_04_18_101530_create_authors_table.php
 ````bash
 $ php artisan make:migration CreateAuthorsTable --create=authors
@@ -9,16 +10,24 @@ $ php artisan make:migration CreateAuthorsTable --create=authors
 $ php artisan migrate
 ````
 
-## Factory
+### Factory
+Crear una factorya para crear Authors
 > database/factories/ModelFactory.php
-````bash
-php artisan make:factory AuthorFactory
-````
 
 
-
-## Seeder
+### Seeder
 Generar automáticamente instancias
 > database/seeds/DatabaseSeeder.php
 
-php artisan make:seeder AuthorsTableSeeder
+````bash
+php artisan db:seed
+````
+
+## Eliminar Datos
+````bash
+php artisan migrate:fresh --seed
+````
+
+# CONTROLLER
+
+> app/Http/Controllers/AuthorController.php
