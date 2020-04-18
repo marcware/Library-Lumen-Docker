20d3e49ee6e7b3540a84b31618da1bda1230c632
Marcos Palacios <m.palacios@geekshubs.com>
2020-04-18 15:58:00 +0200

    Trait

--- LumenAuthorApi/app/Http/Controllers/AuthorController.php
+++ LumenAuthorApi/app/Http/Controllers/AuthorController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class AuthorController extends Controller
 {
+    use ApiResponser;
+
     /**
      * Create a new controller instance.
      *
