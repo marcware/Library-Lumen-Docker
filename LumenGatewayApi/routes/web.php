f9770e2fb26a497c2847b376cee19765736c1657
Marcos Palacios <m.palacios@geekshubs.com>
2020-04-19 18:36:53 +0200

    Configure route groups

--- LumenGatewayApi/routes/web.php
+++ LumenGatewayApi/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Laravel\Lumen\Routing\Router;
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -13,16 +15,31 @@
 
 //$router->get('/', function () use ($router) {return $router->app->version();});
 
-$router->get('/books','BookController@index');
-$router->post('/books','BookController@store');
-$router->get('/books/{book}','BookController@show');
-$router->put('/books/{book}','BookController@update');
-$router->patch('/books/{book}','BookController@update');
-$router->delete('/books/{book}','BookController@destroy');
 
-$router->get('/authors','AuthorController@index');
-$router->post('/authors','AuthorController@store');
-$router->get('/authors/{author}','AuthorController@show');
-$router->put('/authors/{author}','AuthorController@update');
-$router->patch('/authors/{author}','AuthorController@update');
-$router->delete('/authors/{author}','AuthorController@destroy');
+/**
+ * @var Router $router
+ */
+
+$router->group(['middleware' => 'client.credentials'], function () use ($router) {
+    /**
+     * Books routers
+     */
+    $router->get('/books', 'BookController@index');
+    $router->post('/books', 'BookController@store');
+    $router->get('/books/{book}', 'BookController@show');
+    $router->put('/books/{book}', 'BookController@update');
+    $router->patch('/books/{book}', 'BookController@update');
+    $router->delete('/books/{book}', 'BookController@destroy');
+
+    /**
+     * Authors routers
+     */
+    $router->get('/authors', 'AuthorController@index');
+    $router->post('/authors', 'AuthorController@store');
+    $router->get('/authors/{author}', 'AuthorController@show');
+    $router->put('/authors/{author}', 'AuthorController@update');
+    $router->patch('/authors/{author}', 'AuthorController@update');
+    $router->delete('/authors/{author}', 'AuthorController@destroy');
+});
+
+
